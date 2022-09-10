2ff50c9abfc6e4ece3e2a399e76e8eda5584dc1d
JackDelAguila <jackdelaguilavilla03@gmail.com>
2022-09-10 17:42:26 -0500

    add first homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-
-
 public class Main {
     public static int sumatotal(int a , int b , int c){
         return a+b+c;
@@ -23,6 +21,7 @@ public class Main {
         int a,b,c, total;
         a = 3; b = 2; c = 1;
         total = sumatotal(a,b,c);
+        System.out.println(total);
 
         Car carrito = new Car();
         carrito.addDoor();
