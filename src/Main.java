ef8c0f143c2d8ea8dd7803b1ff558e718d1140dc
JackDelAguila <jackdelaguilavilla03@gmail.com>
2022-09-10 17:40:36 -0500

    add first homework

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+
+
+public class Main {
+    public static int sumatotal(int a , int b , int c){
+        return a+b+c;
+    }
+
+    public static class Car {
+        public
+        int doors;
+
+
+        public Car() {
+            this.doors = 0;
+        }
+
+        public void addDoor() {
+            this.doors++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int a,b,c, total;
+        a = 3; b = 2; c = 1;
+        total = sumatotal(a,b,c);
+
+        Car carrito = new Car();
+        carrito.addDoor();
+    }
+
+}
